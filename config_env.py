0e00a11ee8cf609c79bd711a9b5777fd8c90086f
abeSanchez <abraham5001@aol.com>
2020-11-20 14:50:06 -0500

    mes

--- config_env.py
+++ config_env.py
@@ -1,7 +1,7 @@
 # This file configures platform, path and datasets
 import os
 
-PROCESS_UNIT = "gpu"
+PROCESS_UNIT = "cpu"
 
 # Path to caffe
 CAFFE_DIR = os.path.join("/home", "workspace", "caffe")
@@ -11,4 +11,4 @@ IMAGENET_DIR = os.path.join("/home", "datasets", "ILSVRC2012", "Data", "CLS_LOC"
 PLACES205_DIR = os.path.join("/home", "datasets", "Places205")
 PASCAL_VOC_2007_DIR = os.path.join("/home", "datasets", "PASCAL", "VOC2007")
 PASCAL_VOC_2012_DIR = os.path.join("/home", "datasets", "PASCAL", "VOC2012")
-TINY_IMAGENET_DIR = os.path.join("/home", "cap6614.student1", "datasets", "tiny-imagenet-200")
+TINY_IMAGENET_DIR = os.path.join("/Users", "abe", "datasets", "tiny-imagenet-200")
--- pytorch_feature_decoupling/dataloader.py
+++ pytorch_feature_decoupling/dataloader.py
@@ -186,13 +186,13 @@ class DataLoader(object):
             def _load_function(idx):
                 idx = idx % len(self.dataset)
                 img, _, index = self.dataset[idx]
-                #TODO: rotation_labels = torch.LongTensor([0, 1, 2, 3])
-                rotation_labels = torch.LongTensor([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23])
-                #TODO: image_indices = torch.LongTensor([index, index, index, index])
-                image_indices = torch.LongTensor([index, index, index, index, index, index, \
-                                                  index, index, index, index, index, index, \
-                                                  index, index, index, index, index, index, \
-                                                  index, index, index, index, index, index ])
+                rotation_labels = torch.LongTensor([0, 1, 2, 3])
+                #rotation_labels = torch.LongTensor([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23])
+                image_indices = torch.LongTensor([index, index, index, index])
+                #image_indices = torch.LongTensor([index, index, index, index, index, index, \
+                                                #   index, index, index, index, index, index, \
+                                                #   index, index, index, index, index, index, \
+                                                #   index, index, index, index, index, index ])
                 return img, rotation_labels, image_indices
             def _collate_fun(batch):
                 batch = default_collate(batch)
